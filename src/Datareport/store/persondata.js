eabf2ed2f68fda31410d3497983c6ae62042bc14
motoka <1347276498@qq.com>
2017-12-21 23:50:22 +0800

    编写数据报送中人员信息入库

--- src/Datareport/store/persondata.js
+++ src/Datareport/store/persondata.js
@@ -6,9 +6,13 @@ import { SERVICE_API,USER_API,WORKFLOW_API} from '_platform/api';
 
 export const ModalVisible = createAction('人员Modal显示隐藏');
 export const getAllUsers = createFetchAction(`${USER_API}/users/`,[]);
+export const postPersonList = createFetchAction(`${SERVICE_API}/personlist/`, [], "POST");
+export const getOrgList = createFetchAction(`${SERVICE_API}/org-tree/?depth=1`);
 export const actions = {
 	ModalVisible,
-	getAllUsers
+	getAllUsers,
+	postPersonList,
+	getOrgList
 };
 
 export default handleActions({
