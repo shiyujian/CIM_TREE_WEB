f4673cfb54229729bfb3cbcef19d2b9b8b12bbd2
夏 <598363295@qq.com>
2017-12-21 11:10:29 +0800

    修改

--- src/Datareport/components/CostListData/SumSpeedExamine.js
+++ src/Datareport/components/CostListData/SumSpeedExamine.js
@@ -8,9 +8,10 @@ import {UPLOAD_API,SERVICE_API,FILE_API,STATIC_DOWNLOAD_API,SOURCE_API } from '_
 import WorkflowHistory from '../WorkflowHistory'
 import Preview from '../../../_platform/components/layout/Preview';
 import {getUser} from '_platform/auth';
-const {RangePicker} = DatePicker;
+import '../index.less'; 
+
 const RadioGroup = Radio.Group;
-const {Option} = Select
+const { TextArea } = Input;
 @connect(
 	state => {
 		const { platform} = state;
@@ -20,17 +21,17 @@ const {Option} = Select
 		actions: bindActionCreators({ ...actions,...platformActions}, dispatch)
 	})
 )
-export default class SumSpeedExamine extends Component {
-
+export default class Check extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-            wk:null,
+			wk:null,
             dataSource:[],
             opinion:1,//1表示通过 2表示不通过
-		};
-    }
-    async componentDidMount(){
+		}
+	}
+
+	async componentDidMount(){
         const {wk} = this.props
         let dataSource = JSON.parse(wk.subject[0].data)
         this.setState({dataSource,wk})
@@ -43,11 +44,11 @@ export default class SumSpeedExamine extends Component {
    }
    //提交
     async submit(){
-        if(this.state.opinion === 1){
-            await this.passon();
-        }else{
-            await this.reject();
-        }
+        // if(this.state.opinion === 1){
+        //     await this.passon();
+        // }else{
+        //     await this.reject();
+        // }
         this.props.closeModal("cost_sum_spd_visible",false)
         message.info("操作成功")
     }
@@ -68,8 +69,9 @@ export default class SumSpeedExamine extends Component {
         dataSource.map((o) => {
             //创建文档对象
             let doc = o.related_documents.find(x => {
-                return x.rel_type === 'many_jyp_rel'
+                x.rel_type === 'many_jyp_rel'
             })
+            debugger
             if(doc){
                 doclist_p.push({
                     code:doc.code,
@@ -101,11 +103,11 @@ export default class SumSpeedExamine extends Component {
             wplist.push({
                 code:o.code,
                 extra_params:{
-                    rate:o.rate,
-                    check_status:2
+                    rate:o.rate
                 }
             })
         })
+        debugger
         await addDocList({},{data_list:doclist_a});
         await putDocList({},{data_list:doclist_p})
         await updateWpData({},{data_list:wplist});
@@ -115,6 +117,13 @@ export default class SumSpeedExamine extends Component {
         const {wk} = this.props
         const {actions:{deleteWorkflow}} = this.props
         await deleteWorkflow({pk:wk.id})
+        // let executor = {};
+        // let person = getUser();
+        // executor.id = person.id;
+        // executor.username = person.username;
+        // executor.person_name = person.name;
+        // executor.person_code = person.code;
+        // await logWorkflowEvent({pk:wk.id},{state:wk.current[0].id,action:'退回',note:'滚',executor:executor,attachment:null});
     }
     //预览
     handlePreview(index){
@@ -132,76 +141,110 @@ export default class SumSpeedExamine extends Component {
     onChange(e){
         this.setState({opinion:e.target.value})
     }
+
 	render() {
-        const columns = 
-        [{
-            title:'序号',
-			render:(text,record,index) => {
-				return index+1
-			}
-		},{
-			title: '项目/子项目',
-			dataIndex: 'sunproject',
-		  },{
-			title: '单位工程',
-			dataIndex: 'unitproject',
-		  },{
-			title: '工作节点目标',
-			dataIndex: 'nodetarget',
-		  },{
-			title: '完成时间',
-			dataIndex: 'completiontime',
-		  },{
-			title: '支付金额（万元）',
-			dataIndex: 'summoney',
-		  },{
-			title: '累计占比',
-			dataIndex: 'ratio',
-		  },{
-			title: '备注',
-			dataIndex: 'remarks',
-		  }]
-		return (
-            <Modal
-			title="结算进度信息审批表"
-			key={Math.random()}
-            visible={true}
-            width= {1280}
-			footer={null}
-			maskClosable={false}>
-                <div>
-                    <h1 style ={{textAlign:'center',marginBottom:20}}>结果审核</h1>
-                    <Table style={{ marginTop: '10px', marginBottom:'10px' }}
-                        columns={columns}
-                        dataSource={this.state.dataSource}
-                        bordered />
-                    <Row>
-                        <Col span={2}>
-                            <span>审查意见：</span>
-                        </Col>
-                        <Col span={4}>
-                            <RadioGroup onChange={this.onChange.bind(this)} value={this.state.opinion}>
-                                <Radio value={1}>通过</Radio>
-                                <Radio value={2}>不通过</Radio>
-                            </RadioGroup>
-                        </Col>
-                        <Col span={2} push={14}>
-                            <Button type='primary'>
-                                导出表格
-                            </Button>
-                        </Col>
-                        <Col span={2} push={14}>
-                            <Button type='primary' onClick={this.submit.bind(this)}>
-                                确认提交
-                            </Button>
-                            <Preview />
-                        </Col>
-                    </Row>
-                    {
-                        this.state.wk && <WorkflowHistory wk={this.state.wk}/>
-                    }
-                </div>
-            </Modal>
+		return(
+			<Modal
+				width = {1280}
+				visible = {true}
+				onCancel = {this.cancel.bind(this)}
+			>
+				<Row style={{margin: '20px 0', textAlign: 'center'}}>
+					<h2>结果审核</h2>
+				</Row>
+				<Row>
+					<Table
+						bordered
+						className = 'foresttable'
+						columns={this.columns}
+					/>
+				</Row>
+				<Row style={{margin: '20px 0'}}>
+					<Col span={2}>
+						<span>审查意见：</span>
+					</Col>
+					<Col span={4}>
+						<RadioGroup onChange={this.onChange} value={this.state.value}>
+					        <Radio value={1}>通过</Radio>
+					        <Radio value={2}>不通过</Radio>
+					    </RadioGroup>
+				    </Col>
+				    <Col span={2} push={14}>
+				    	<Button type='primary'>
+        					导出表格
+        				</Button>
+				    </Col>
+				    <Col span={2} push={14}>
+				    	<Button type='primary' onClick={this.submit.bind(this)}>
+        					确认提交
+        				</Button>
+				    </Col>
+			    </Row>
+			    <Row style={{margin: '20px 0'}}>
+				    <Col>
+				    	<TextArea rows={2} />
+				    </Col>
+			    </Row>
+			    <Row style={{marginBottom: '10px'}}>
+			    	<div>审批流程</div>
+			    </Row>
+			    <Row>
+			    	<Col span={10}>
+			    		<div style={{padding: '20px 0 0 10px', width: '300px', height: '200px', border: '1px solid #000'}}>
+			    			<div>执行人：数据上传者</div>
+			    			<div>执行时间：2017-11-22</div>
+			    			<div>执行意见：XXXXXXXXXXXXX</div>
+			    			<div style={{marginTop: '40px'}}>电子签章：</div>
+			    		</div>
+			    		<div style={{width: '300px', textAlign: 'center', fontSize: '16px'}}>数据上传</div>
+			    	</Col>
+			    	<Col span={10}>
+			    		<div style={{padding: '20px 0 0 10px', width: '300px', height: '200px', border: '1px solid #000'}}>
+			    			<div>执行人：数据审批</div>
+			    			<div>执行时间：</div>
+			    			<div>执行意见：</div>
+			    			<div style={{marginTop: '40px'}}>电子签章：</div>
+			    		</div>
+			    		<div style={{width: '300px', textAlign: 'center', fontSize: '16px'}}>数据上传审核</div>
+			    	</Col>
+			    </Row>
+			</Modal>
 		)
-    }
+	}
+
+	columns =
+                [{
+                    title:'序号',
+                    render:(text,record,index) => {
+                    return index+1
+                }
+            },{
+                title: '项目/子项目',
+                    dataIndex: 'sunproject',
+            },{
+                title: '单位工程',
+                    dataIndex: 'unitproject',
+            },{
+                title: '工作节点目标',
+                    dataIndex: 'nodetarget',
+            },{
+                title: '完成时间',
+                    dataIndex: 'completiontime',
+            },{
+                title: '支付金额（万元）',
+                    dataIndex: 'summoney',
+            },{
+                title: '累计占比',
+                    dataIndex: 'ratio',
+            },{
+                title: '备注',
+                    dataIndex: 'remarks',
+            }];
+
+	cancel() {
+		const {
+			actions: {clearCheckField}
+		} = this.props;
+		clearCheckField();
+	}
 }
