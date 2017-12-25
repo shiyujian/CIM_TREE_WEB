047e7f7e2959f24dd3d92b25ad31535e12800f49
zhangyu <mrxyzhang@163.com>
2017-12-25 21:02:31 +0800

    修改文件名称

--- src/Datareport/components/CostListData/SumPlanDelateCheck.js
+++ src/Datareport/components/CostListData/SumPlanDelateCheck.js
@@ -23,7 +23,7 @@ const {Option} = Select;
 		actions: bindActionCreators({ ...actions,...platformActions}, dispatch)
 	})
 )
-export default class SumSpeedExamineDelete extends Component {
+export default class SumPlanDelateCheck extends Component {
 
 	constructor(props) {
 		super(props);
