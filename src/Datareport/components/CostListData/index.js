f51e594b052d39a59d0a479d31a3dc4c422462b1
zhangyu <mrxyzhang@163.com>
2017-12-25 21:01:10 +0800

    结算计划，数据展示，选择删除页面

--- src/Datareport/components/CostListData/index.js
+++ src/Datareport/components/CostListData/index.js
@@ -2,5 +2,6 @@ export PriceList from "./PriceList";
 export PriceListCheck from "./PriceListCheck";
 export ProjectSum from "./ProjectSum";
 export SumPlan from "./SumPlan";
+export SumPlanDelate from "./SumPlanDelate";
 export SumSpeed from "./SumSpeed";
 export ProjectSumExamine from "./ProjectSumExamine";
