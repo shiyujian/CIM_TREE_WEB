437d9412447c3a2321729f02af42b63e631b5840
寇晓阳 <654113183@qq.com>
2017-12-26 15:42:39 +0800

    新增模型信息的删除功能

--- src/Datareport/components/ModalData/ExpurgateCheck.js
+++ src/Datareport/components/ModalData/ExpurgateCheck.js
@@ -1,68 +1,70 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import {actions as platformActions} from '_platform/store/global';
-import {Input,Col, Card,Table,Row,Button,DatePicker,Radio,Select,notification,Popconfirm,Modal,Upload,Icon,message} from 'antd';
-import {UPLOAD_API,SERVICE_API,FILE_API,STATIC_DOWNLOAD_API,SOURCE_API } from '_platform/api';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actions as platformActions } from '_platform/store/global';
+import { Input, Col, Card, Table, Row, Button, DatePicker, Radio, Select, notification, Popconfirm, Modal, Upload, Icon, message } from 'antd';
+import { UPLOAD_API, SERVICE_API, FILE_API, STATIC_DOWNLOAD_API, SOURCE_API } from '_platform/api';
 import WorkflowHistory from '../WorkflowHistory';
-import {getUser} from '_platform/auth';
-import {actions} from '../../store/safety';
+import { getUser } from '_platform/auth';
+import { actions } from '../../store/safety';
 import Preview from '../../../_platform/components/layout/Preview';
 import moment from 'moment';
 
-const {RangePicker} = DatePicker;
+const { RangePicker } = DatePicker;
 const RadioGroup = Radio.Group;
-const {Option} = Select;
+const { Option } = Select;
 
 @connect(
-	state => {
-        const {datareport: {safety = {}} = {}, platform} = state;
-		return {...safety, platform}
-	},
-	dispatch => ({
-		actions: bindActionCreators({ ...actions,...platformActions}, dispatch)
-	})
+    state => {
+        const { datareport: { ModalData = {} } = {}, platform } = state;
+        return { ...ModalData, platform }
+    },
+    dispatch => ({
+        actions: bindActionCreators({ ...actions, ...platformActions }, dispatch)
+    })
 )
 export default class ExpurgateCheck extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-            wk:null,
-            dataSource:[],
-            option:1,
-		};
+    constructor(props) {
+        super(props);
+        this.state = {
+            wk: null,
+            dataSource: [],
+            option: 1,
+        };
     }
-    async componentDidMount(){
-        const {wk} = this.props
-        let dataSource = JSON.parse(wk.subject[0].data)
-        this.setState({dataSource,wk});
+    async componentDidMount() {
+        const { wk } = this.props
+        let dataSources = JSON.parse(wk.subject[0].data)
+        console.log('asdfxsfdgg:',dataSources)
+        let dataSource = [];
+        dataSources.map(item => {
+            dataSource.push(item)
+        })
+        this.setState({ dataSource, wk });
     }
 
-    componentWillReceiveProps(props){
-        const {wk} = props
-        let dataSource = JSON.parse(wk.subject[0].data)
-        this.setState({dataSource,wk})
-   }
-   //提交
-    async submit(){
-        if(this.state.option === 1){
+
+    //提交
+    async submit() {
+        if (this.state.option === 1) {
             await this.passon();
-        }else{
+        } else {
             await this.reject();
         }
-        this.props.closeModal("expurgate_check_visbile",false);
+        this.props.closeModal("expurgate_check_visbile", false);
         message.info("操作成功");
     }
 
     //通过
-    async passon(){
-        const {dataSource,wk,topDir} = this.state;
-        const {actions:{
+    async passon() {
+        const { dataSource, wk, topDir } = this.state;
+        console.log('da',dataSource)
+        const { actions: {
             logWorkflowEvent,
             delDocList
-        }} = this.props;
-        
+        } } = this.props;
+
         // send workflow
         let executor = {};
         let person = getUser();
@@ -70,15 +72,21 @@ export default class ExpurgateCheck extends Component {
         executor.username = person.username;
         executor.person_name = person.name;
         executor.person_code = person.code;
-        await logWorkflowEvent({pk:wk.id},{state:wk.current[0].id,action:'通过',note:'同意',executor:executor,attachment:null});
-        
-        
-        if(rst.result){
+        await logWorkflowEvent({ pk: wk.id }, { state: wk.current[0].id, action: '通过', note: '同意', executor: executor, attachment: null });
+         
+        const docCode = [];
+        dataSource.map(item=>{
+            docCode.push(item.code);
+        })
+       
+
+        let rst = await delDocList({}, { code_list: docCode });
+        if (rst.result) {
             notification.success({
                 message: '删除文档成功！',
                 duration: 2
             });
-        }else{
+        } else {
             notification.error({
                 message: '删除文档失败！',
                 duration: 2
@@ -86,87 +94,76 @@ export default class ExpurgateCheck extends Component {
         }
     }
     //不通过
-    async reject(){
-        const {wk} = this.props
-        const {actions:{deleteWorkflow}} = this.props
-        await deleteWorkflow({pk:wk.id})
+    async reject() {
+        const { wk } = this.props
+        const { actions: { deleteWorkflow } } = this.props
+        await deleteWorkflow({ pk: wk.id })
     }
-    onChange(e){
-        this.setState({option:e.target.value})
+    onChange(e) {
+        this.setState({ option: e.target.value })
     }
-	render() {
-        console.log(this.state.dataSource)
+    render() {
+        console.log('wadsdf', this.state.dataSource)
+
         const columns = [{
-			title: '序号',
-			dataIndex: 'numbers',
-			render: (text, record, index) => {
-				return index + 1
-			}
-		}, {
-			title: '模型编码',
-			dataIndex: 'coding'
-		}, {
-			title: '项目/子项目名称',
-			dataIndex: 'project'
-		}, {
-			title: '单位工程',
-			dataIndex: 'unit'
-		}, {
-			title: '模型名称',
-			dataIndex: 'modelName'
-		}, {
-			title: '提交单位',
-			dataIndex: 'submittingUnit'
-		}, {
-			title: '模型描述',
-			dataIndex: 'modelDescription'
-		}, {
-			title: '模型类型',
-			dataIndex: 'modeType'
-		}, {
-			title: 'fdb模型',
-			dataIndex: 'fdbMode',
-			
-		}, {
-			title: 'tdbx模型',
-			dataIndex: 'tdbxMode',
-			
-		}, {
-			title: '属性表',
-			dataIndex: 'attributeTable',
-		
-		}, {
-			title: '上报时间',
-			dataIndex: 'reportingTime'
-		}, {
-			title: '上报人',
-			dataIndex: 'reportingName'
-		}, {
-			title: '操作',
-			render: (text, record, index) => {
-				return (
-					<Popconfirm
-						placement="leftTop"
-						title="确定删除吗？"
-						onConfirm={this.delete.bind(this, index)}
-						okText="确认"
-						cancelText="取消">
-						<a>删除</a>
-					</Popconfirm>
-				)
-			}
-		}];
-          
-          
-		return (
+            title: '序号',
+            dataIndex: 'numbers',
+            render: (text, record, index) => {
+                return index + 1
+            }
+        }, {
+            title: '模型编码',
+            dataIndex: 'coding'
+        }, {
+            title: '项目/子项目名称',
+            dataIndex: 'project'
+        }, {
+            title: '单位工程',
+            dataIndex: 'unit'
+        }, {
+            title: '模型名称',
+            dataIndex: 'modelName'
+        }, {
+            title: '提交单位',
+            dataIndex: 'submittingUnit'
+        }, {
+            title: '模型描述',
+            dataIndex: 'modelDescription'
+        }, {
+            title: '模型类型',
+            dataIndex: 'modeType'
+        }, {
+            title: 'fdb模型',
+            dataIndex: 'fdbMode',
+
+        }, {
+            title: 'tdbx模型',
+            dataIndex: 'tdbxMode',
+
+        }, {
+            title: '属性表',
+            dataIndex: 'attributeTable',
+
+        }, {
+            title: '上报时间',
+            dataIndex: 'reportingTime'
+        }, {
+            title: '上报人',
+            dataIndex: 'reportingName'
+        }];
+
+
+        return (
             <Modal
-			title="模型信息删除审批表"
-            visible={true}
-            width= {1280}
-			footer={null}
-			maskClosable={false}>
-                <h1 style ={{textAlign:'center',marginBottom:20}}>结果审核</h1>
-                <Table style={{ marginTop: '10px', marginBottom:'10px' }}
+                title="模型信息删除审批表"
+                footer={null}
+                visible={true}
+                width={1280}
+                onCancel={this.cancel.bind(this)}
+
+            >
+                <h1 style={{ textAlign: 'center', marginBottom: 20 }}>结果审核</h1>
+                <Table style={{ marginTop: '10px', marginBottom: '10px' }}
                     columns={columns}
                     dataSource={this.state.dataSource}
                     bordered />
@@ -193,9 +190,13 @@ export default class ExpurgateCheck extends Component {
                     </Col>
                 </Row>
                 {
-                    this.state.wk && <WorkflowHistory wk={this.state.wk}/>
+                    this.state.wk && <WorkflowHistory wk={this.state.wk} />
                 }
             </Modal>
-		)
+        )
+    }
+
+    cancel() {
+        this.props.closeModal("expurgate_check_visbile", false)
     }
 }
