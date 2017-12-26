5c6812c34f4a34db27004d7a71d8a5680bc06f04
宋锡铨 <1044955115@qq.com>
2017-12-26 10:15:37 +0800

    单位入库显示，删除界面

--- src/Datareport/components/UnitData/TableUnit.js
+++ src/Datareport/components/UnitData/TableUnit.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import {Table,Button,Popconfirm,message,Input,Icon} from 'antd';
-import style from './TableUnit.css'
+import style from './TableUnit.css';
+import DelModal from './DelModal'
 const Search = Input.Search;
 export default class TableUnit extends Component{
 	constructor(props){
 		super(props);
-		this.state = {}
+		this.state = {
+			deling:false
+		}
 	}
 	async componentDidMount(){
 		let {getProjectAcD3,getDocByCodeList,getDocByCodeSearcher} = this.props.actions
@@ -19,7 +22,8 @@ export default class TableUnit extends Component{
 		docs.result.forEach(doc=>{
 			docSet[doc.code] = doc;
 		});
-		units = units.map(unit=>{
+		units = units.map((unit,index)=>{
+			unit.key = index;
 			unit.children = null;
 			if(docSet[unit.code+'REL_DOC_DW_A']){
 				return{...unit,...unit.extra_params,...docSet[unit.code+'REL_DOC_DW_A'].extra_params,...docSet[unit.code+'REL_DOC_DW_A'].basic_params}
@@ -30,23 +34,54 @@ export default class TableUnit extends Component{
 		this.setState({units:units})
 	}
     render(){
+		let rowSelection = {
+			selectedRowKeys:this.state.selectedRowKeys||[],
+			onChange: (selectedRowKeys, selectedRows) => {
+				this.setState({selectedRowKeys:selectedRowKeys,selectedRows});
+			  console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+			},
+			onSelect: (record, selected, selectedRows) => {
+			  console.log(record, selected, selectedRows);
+			},
+			onSelectAll: (selected, selectedRows, changeRows) => {
+			  console.log(selected, selectedRows, changeRows);
+			},
+		  };
         return (
             <div>
                 <div>
                 <Button style={{ marginRight: "10px" }}  className = {style.button}>模板下载</Button>
                     <Button  className = {style.button} onClick = {this.send.bind(this)}>发送填报</Button>
                     <Button className = {style.button}>申请变更</Button>
-                    <Button className = {style.button}>申请删除</Button>
+                    <Button onClick = {()=>{
+						if(this.state.selectedRows && this.state.selectedRows.length>0)
+						{
+							this.setState({deling:true});
+							return;
+						}
+						message.warning('请至少选择一条');
+					}} className = {style.button}>申请删除</Button>
                     <Button className = {style.button}>导出表格</Button>
                     <Search className = {style.button} style={{width:"200px"}} placeholder="请输入内容" />
                 </div>
                 <Table
                     columns = {this.columns}
                     bordered = {true}
-                    rowSelection={this.rowSelection}
+                    rowSelection={rowSelection}
                     dataSource = {this.state.units||[]}
                 >
-                </Table>
+				</Table>
+				{
+					this.state.deling &&
+					<DelModal
+						onCancel = {()=>{
+							this.setState({deling:false});
+						}}
+						dataSource={this.state.selectedRows}
+						actions={this.props.actions}
+					/>
+				}
+
             </div>
         )
     }
@@ -56,17 +91,7 @@ export default class TableUnit extends Component{
       ModalVisibleUnit(true);
      
     }
-    rowSelection = {
-      onChange: (selectedRowKeys, selectedRows) => {
-        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-      },
-      onSelect: (record, selected, selectedRows) => {
-        console.log(record, selected, selectedRows);
-      },
-      onSelectAll: (selected, selectedRows, changeRows) => {
-        console.log(selected, selectedRows, changeRows);
-      },
-    };
+
 
     columns = [{
         title: '单位工程编码',
@@ -103,7 +128,13 @@ export default class TableUnit extends Component{
         key:'Intro'
       },{
         title: '建设单位',
-		dataIndex:'relOrg',
+        render:(record)=>{
+			let ogrname = '';
+			if(record.rsp_orgName && record.rsp_orgName.length>0){
+				ogrname =record.rsp_orgName[0];
+			}
+			return (<span>{ogrname}</span>)
+		},
         key:'Org'
       },{
           title:'附件',
