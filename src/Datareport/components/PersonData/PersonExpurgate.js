03260dff4666f30b0fe2c388a7b77a43c3b8b5d1
谢洪禹 <1040325106@qq.com>
2018-01-04 14:52:06 +0800

    人员信息：修改细节

--- src/Datareport/components/PersonData/PersonExpurgate.js
+++ src/Datareport/components/PersonData/PersonExpurgate.js
@@ -122,16 +122,7 @@ export default class PersonExpurgate extends Component {
                     </Select>
 
                 </span>
-                <Row style={{marginBottom: '10px'}}>
-                    <Col span={2}>
-                        <span>删除原因：</span>
-                    </Col>
-                </Row>
-                <Row style={{margin: '10px 0'}}>
-                    <Col>
-                        <TextArea rows={2} onChange={this.description.bind(this)}/>
-                    </Col>
-                </Row>
+                <TextArea rows={2} style={{margin: '10px 0'}} onChange={this.description.bind(this)}  placeholder='请输入删除原因'/>
             </Modal>
         )
 	}
