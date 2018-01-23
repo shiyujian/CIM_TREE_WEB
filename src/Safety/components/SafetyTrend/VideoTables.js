5949eb490e69342cd1dc13e623aa5b23364a95a6
lose <193382082@qq.com>
2018-01-23 15:13:46 +0800

    修改项目管理 安全动态：安全生产视频 安全动态管理的安全生产视频

---
+++ src/Safety/components/SafetyTrend/VideoTables.js
@@ -0,0 +1,348 @@
+import React, { Component } from 'react';
+import { Table, Tabs, Button, Row, Col, Modal, message, Popconfirm } from 'antd';
+import VideoText from './VideoText';
+import moment from 'moment';
+import { getUser } from '../../../_platform/auth';
+
+const TabPane = Tabs.TabPane;
+const user_id = getUser().id;
+
+export default class VideoTables extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			visible: false,
+			container: null,
+			video: [],
+			file: {},
+			down_file:''
+		}
+	}
+
+	componentDidMount() {
+		const { actions: { getNewsList, getDraftNewsList } } = this.props;
+		getNewsList({
+			user_id: user_id
+		})
+		getDraftNewsList({
+			user_id: user_id
+		})
+		// this.state.video.push(this.state.file)
+		console.log(this.props, "333333333333")
+		// console.log(this.state.video)
+
+		const newsLists = this.props.tipsList || []
+		for (var i = 0; i < newsLists.length; i++) {
+			// console.log(newsLists[i])
+			// console.log(newsLists[i].attachment.fileList,"1111111111111")			
+			if (newsLists[i].attachment.fileList != undefined) {
+				console.log(newsLists[i].attachment.fileList[0], "222222222222222222")
+				if (newsLists[i].attachment.fileList[0] != undefined) {
+					this.state.file = newsLists[i].attachment.fileList[0]
+					this.state.video.push(this.state.file)
+					console.log(this.props, "333333333333")
+					console.log(this.state.video,"444444444444444")
+				}
+			}
+		}
+	}
+	// componentWillReceiveProps(nextProps) {
+	// 	let btn=true		
+	// 	console.log(nextProps)
+	// 	const newsLists = nextProps.tipsList || []
+	// 	for (var i = 0; i < newsLists.length; i++) {
+	// 		// console.log(newsLists[i])
+	// 		// console.log(newsLists[i].attachment.fileList,"1111111111111")			
+	// 		if (newsLists[i].attachment.fileList != undefined) {
+	// 			console.log(newsLists[i].attachment.fileList[0], "222222222222222222")
+	// 			if (newsLists[i].attachment.fileList[0] != undefined) {
+	// 				this.state.file = newsLists[i].attachment.fileList[0]
+	// 				this.state.video.push(this.state.file)
+	// 					// console.log(this.props, "333333333333")
+	// 					console.log(this.state.video,"444444444444444")
+
+	// 			}
+	// 		}
+	// 	}
+	// 	console.log(btn)
+	// 	  btn=false
+	// 	console.log(btn)
+		
+	// }
+
+	//新闻操作按钮
+	clickNews(record, type) {
+		console.log(record)
+		this.setState({down_file:record.down_file})
+		const {
+			actions: { deleteData, getNewsList, getDraftNewsList, toggleModal, patchData },
+			bulletinTabValue = '1'
+		} = this.props;
+		if (type === 'DELETE') {
+			deleteData({ pk: record.id })
+				.then(() => {
+					message.success('删除新闻成功！');
+					if (bulletinTabValue === '1') {
+						getNewsList({
+							user_id: user_id
+						});
+					} else {
+						getDraftNewsList({
+							user_id: user_id
+						});
+					}
+				})
+		} else if (type === 'EDIT') {
+			toggleModal({
+				type: 'NEWS',
+				status: 'EDIT',
+				visible: true,
+				editData: record,
+			})
+		} else if (type === 'VIEW') {
+			this.setState({
+				visible: true,
+				container: record.raw
+			})
+		} else if (type === 'BACK') {
+			let newData = {
+				"update_time": moment().format('YYYY-MM-DD HH:mm:ss'),
+				"is_draft": true
+			};
+			patchData({ pk: record.id }, newData)
+				.then(rst => {
+					if (rst.id) {
+						message.success('撤回成功，撤回的新闻在暂存的新闻中可查看');
+						//更新暂存的新闻列表数据
+						getNewsList({
+							user_id: user_id
+						});
+						getDraftNewsList({
+							user_id: user_id
+						});
+					}
+				})
+		} else if (type === 'PUBLISH') {
+			let newData = {
+				"update_time": moment().format('YYYY-MM-DD HH:mm:ss'),
+				"is_draft": false
+			};
+			patchData({ pk: record.id }, newData)
+				.then(rst => {
+					if (rst.id) {
+						message.success('重新发布新闻成功！');
+						//更新暂存的新闻列表数据
+						getNewsList({
+							user_id: user_id
+						});
+						getDraftNewsList({
+							user_id: user_id
+						});
+					}
+				})
+		}
+
+	}
+
+	//发布新闻
+	publishNewsClick() {
+		const { actions: { toggleModal } } = this.props;
+		toggleModal({
+			type: 'NEWS',
+			status: 'ADD',
+			visible: true,
+			editData: null
+		})
+
+
+	}
+
+	handleCancel() {
+		this.setState({
+			visible: false,
+			container: null,
+		})
+	}
+
+	//新闻列表和暂存的新闻列表切换
+	subTabChange(bulletinTabValue) {
+		const { actions: { setBulletinTabActive } } = this.props;
+		setBulletinTabActive(bulletinTabValue);
+	}
+
+	render() {
+		const {
+			newsList = [],
+			draftNewsLis = [],
+			toggleData: toggleData = {
+				type: 'NEWS',
+				visible: false,
+			},
+			bulletinTabValue = '1'
+		} = this.props;
+		// const data = [{
+		// 	name: this.state.video.name
+		// }]
+		// console.log(this.state.video)
+		return (
+			<Row>
+				<Col span={22} offset={1}>
+					<Tabs activeKey={bulletinTabValue} onChange={this.subTabChange.bind(this)} tabBarExtraContent={
+						<div style={{ marginBottom: '10px' }}>
+							<Button type="primary" onClick={this.publishNewsClick.bind(this)}>发布安全事故快报</Button>
+							{
+								(toggleData.visible && toggleData.type === 'NEWS') && <VideoText {...this.props} />
+							}
+						</div>}>
+						<TabPane tab="发布的视频" key="1">
+							<Table dataSource={newsList}
+								columns={this.columns}
+								rowKey="id" />
+						</TabPane>
+						<TabPane tab="暂存的视频" key="2">
+							<Table dataSource={draftNewsLis}
+								columns={this.draftColumns}
+								rowKey="id" />
+						</TabPane>
+					</Tabs>
+
+				</Col>
+				<Modal
+					title="新闻预览"
+					width="800px"
+					visible={this.state.visible}
+					onOk={this.handleCancel.bind(this)}
+					onCancel={this.handleCancel.bind(this)}
+					footer={null}>
+						<video
+						controls
+						preload="auto"
+						width="100%"
+						height="500px"
+						src={this.state.down_file}
+					>
+					</video>
+				</Modal>
+			</Row>
+		);
+
+	}
+
+	// columns1 = [{
+	// 	title: "视频名称",
+	// 	dataIndex: "name",
+	// }, {
+	// 	title: '操作',
+	// 	render: record => {
+	// 		return (
+	// 			<span>
+	// 				<a onClick={this.clickNews.bind(this, record, 'VIEW')}>预览</a>
+	// 				&nbsp;&nbsp;|&nbsp;&nbsp;
+	// 				<a onClick={this.clickNews.bind(this, record, 'EDIT')}>修改</a>
+	// 				&nbsp;&nbsp;|&nbsp;&nbsp;
+	// 				<a onClick={this.clickNews.bind(this, record, 'BACK')}>撤回</a>
+	// 				&nbsp;&nbsp;|&nbsp;&nbsp;
+	// 				<Popconfirm title="确定删除吗?" onConfirm={this.clickNews.bind(this, record, 'DELETE')} okText="确定"
+	// 					cancelText="取消">
+	// 					<a>删除</a>
+	// 				</Popconfirm>
+	// 			</span>
+	// 		)
+	// 	},
+	// }]
+
+	columns = [
+		{
+			title: '新闻ID222',
+			dataIndex: 'id',
+			key: 'id',
+		}, {
+			title: '新闻标题',
+			dataIndex: 'title',
+			key: 'title',
+		}, {
+			title: '关键字',
+			dataIndex: 'abstract',
+			key: 'abstract',
+		}, {
+			title: '发布时间',
+			dataIndex: 'pub_time',
+			key: 'pub_time',
+			render: pub_time => {
+				return moment(pub_time).utc().format('YYYY-MM-DD HH:mm:ss');
+			}
+		}, {
+			title: '更新时间',
+			dataIndex: 'update_time',
+			key: 'update_time',
+			render: update_time => {
+				return moment(update_time).utc().format('YYYY-MM-DD HH:mm:ss');
+			}
+		}, {
+			title: '操作',
+			render: record => {
+				return (
+					<span>
+						<a onClick={this.clickNews.bind(this, record, 'VIEW')}>查看</a>
+						&nbsp;&nbsp;|&nbsp;&nbsp;
+						<a onClick={this.clickNews.bind(this, record, 'EDIT')}>修改</a>
+						&nbsp;&nbsp;|&nbsp;&nbsp;
+						<a onClick={this.clickNews.bind(this, record, 'BACK')}>撤回</a>
+						&nbsp;&nbsp;|&nbsp;&nbsp;
+						<Popconfirm title="确定删除吗?" onConfirm={this.clickNews.bind(this, record, 'DELETE')} okText="确定"
+							cancelText="取消">
+							<a>删除</a>
+						</Popconfirm>
+					</span>
+				)
+			},
+		}
+	];
+	draftColumns = [
+		{
+			title: '新闻ID',
+			dataIndex: 'id',
+			key: 'id',
+		}, {
+			title: '新闻标题',
+			dataIndex: 'title',
+			key: 'title',
+		}, {
+			title: '关键字',
+			dataIndex: 'abstract',
+			key: 'abstract',
+		}, {
+			title: '暂存时间',
+			dataIndex: 'pub_time',
+			key: 'pub_time',
+			render: pub_time => {
+				return moment(pub_time).utc().format('YYYY-MM-DD HH:mm:ss');
+			}
+		}, {
+			title: '撤回时间',
+			dataIndex: 'update_time',
+			key: 'update_time',
+			render: update_time => {
+				return moment(update_time).utc().format('YYYY-MM-DD HH:mm:ss');
+			}
+		}, {
+			title: '操作',
+			render: record => {
+				return (
+					<span>
+						<a onClick={this.clickNews.bind(this, record, 'PUBLISH')}>发布</a>
+						&nbsp;&nbsp;|&nbsp;&nbsp;
+						<a onClick={this.clickNews.bind(this, record, 'VIEW')}>查看</a>
+						&nbsp;&nbsp;|&nbsp;&nbsp;
+						<a onClick={this.clickNews.bind(this, record, 'EDIT')}>修改</a>
+						&nbsp;&nbsp;|&nbsp;&nbsp;
+						<Popconfirm title="确定删除吗?" onConfirm={this.clickNews.bind(this, record, 'DELETE')} okText="确定"
+							cancelText="取消">
+							<a>删除</a>
+						</Popconfirm>
+					</span>
+				)
+			},
+		}
+	];
+}
--- src/Safety/components/Trend/Video.js
+++ src/Safety/components/Trend/Video.js
@@ -1,9 +1,12 @@
+// import './video.less'
+
 import React, { Component } from 'react';
 import { Table, Row, Col, Modal } from 'antd';
 import Blade from '_platform/components/panels/Blade';
 import moment from 'moment';
 import $ from 'jquery';
 import { Icon } from 'react-fa';
+import './video.less'
 export default class Video extends Component {
 
 	constructor(props) {
@@ -28,7 +31,7 @@ export default class Video extends Component {
 			const newsListTT = newsListT[i].attachment.fileList || []
 			for (var j = 0; j < newsListTT.length; j++) {
 				console.log(newsListTT[j])
-				const videos = newsListTT[3].down_file
+				const videos = newsListTT[j].down_file
 				this.setState({ video: videos })
 				break;
 			}
@@ -44,11 +47,68 @@ export default class Video extends Component {
 		console.log(this.props.getNewsList)
 		getNewsList({}, { tag: '新闻', is_draft: false });
 
-		$(".box").mouseover(function(){
-			$(".box").css({"background-color":"#eee",zIndex:"100"})
+		// // 视频一
+		// $(".firstVideo").mouseover(function () {
+		// 	$(".firstVideo").css({ "background-color": "#eee", zIndex: "100" })
+		// 	$(".firstIcon").css({ display: "block", zIndex: "100" })
+		// })
+		// $(".firstIcon").mouseover(function () {
+		// 	$(".firstVideo").css({ "background-color": "#eee", zIndex: "100" })
+		// 	$(".firstIcon").css({ display: "block", zIndex: "100" })
+		// })
+		// $(".firstVideo").mouseout(function () {
+		// 	$(".firstVideo").css({ "background-color": "", zIndex: "0" })
+		// 	$(".firstIcon").css({ display: "none", zIndex: "0" })
+
+		// })
+		var boxs=document.getElementsByClassName("box")[0]
+		console.log(boxs)
+		boxs.onmouseover=function(){
+			console.log("1111")
+			boxs.classList.add("box")
+		}
+		boxs.onmouseout=function(){
+			console.log("2222")
+			boxs.classList.remove("box")
+		}
+
+		// // 视频二
+		// $(".secondVideo").mouseover(function () {
+		// 	$(".secondVideo").css({ "background-color": "#eee", zIndex: "100" })
+		// 	$(".secondIcon").css({ display: "block", zIndex: "101" })
+		// })
+		// $(".secondIcon").mouseover(function () {
+		// 	$(".secondVideo").css({ "background-color": "#eee", zIndex: "100" })
+		// 	$(".secondIcon").css({ display: "block", zIndex: "101" })
+		// })
+		// $(".secondVideo").mouseout(function () {
+		// 	$(".secondVideo").css({ "background-color": "", zIndex: "0" })
+		// 	$(".secondIcon").css({ display: "none", zIndex: "0" })
+
+		// })
+		// 视频三
+		// $(".thirdVideo").mouseover(function () {
+		// 	$(".thirdVideo").css({ "background-color": "#eee", zIndex: "100" })
+		// 	$(".thirdIcon").css({ display: "block", zIndex: "101" })
+		// })
+		// $(".thirdIcon").mouseover(function () {
+		// 	$(".thirdVideo").css({ "background-color": "#eee", zIndex: "100" })
+		// 	$(".thirdIcon").css({ display: "block", zIndex: "101" })
+		// })
+		// $(".thirdVideo").mouseout(function () {
+		// 	$(".thirdVideo").css({ "background-color": "", zIndex: "0" })
+		// 	$(".thirdIcon").css({ display: "none", zIndex: "0" })
+
+		// })
+	}
+	play() {
+		this.setState({
+			visible: true
 		})
-		$(".box").mouseout(function(){
-			$(".box").css({"background-color":"",zIndex:"0"})
+	}
+	modalClick() {
+		this.setState({
+			visible: false
 		})
 	}
 
@@ -98,51 +158,128 @@ export default class Video extends Component {
 	}
 
 	render() {
-		
+
 		console.log(this.state.video)
 		return (
-			<Blade title="安全生产视频">
-				<Row>
-					<Col span={16}>
+			<Blade style={{ height: "300px", background: "#eee" }} title="安全生产视频">
+				{/* <Row >
+					<Col style={{ height: "300px" }} span={16}>
 						<video
-							className="box"
+							style={{ position: "relative" }}
+							onClick={this.play.bind(this)}
+							className="firstVideo"
 							preload="auto"
-							width="300px"
-							height="300px"
-							src={"http://47.104.160.65:6510/media/documents/2018/01/1510116943014_APkNixj.mp4"}
+							width="100%"
+							height="100%"
+							src={this.state.video}
 						>
 						</video>
-						<Icon style={{widtg:"30px",height:"30px"}} className={"playBtn Icon Icon-play_b"}></Icon>
+						<Icon className="firstIcon" 
+						onClick={this.play.bind(this)}
+						style={{ fontSize: 60, width: "60px", height: "60px", display: "none", position: "absolute", top: "0", bottom: "0", left: "0", right: "0", margin: "auto" }} name="play-circle"></Icon>
 					</Col>
-					<Col span={8}>
-						<Row>
+					<Col style={{ height: "300px" }} span={8}>
+						<Row style={{ height: "150px" }}>
 							<video
+								style={{ position: "relative" }}
+								onClick={this.play.bind(this)}
+								className="secondVideo"
 								preload="auto"
-								width="200px"
-								height="150px"
-								src={"http://47.104.160.65:6510/media/documents/2018/01/1510116943014_APkNixj.mp4"}
+								width="100%"
+								height="100%"
+								src={this.state.video}
 
 							>
 							</video>
+							<Icon className="secondIcon" 
+							onClick={this.play.bind(this)}
+							style={{ fontSize: 30, width: "30px", height: "30px", display: "none", position: "absolute", top: "0", bottom: "0", left: "0", right: "0", margin: "auto" }} name="play-circle"></Icon>
 						</Row>
-						<Row>
+						<Row style={{ height: "150px" }}>
 							<video
+								style={{ position: "relative" }}
+								onClick={this.play.bind(this)}
+								className="thirdVideo"
 								preload="auto"
-								width="200px"
-								height="150px"
-								src={"http://47.104.160.65:6510/media/documents/2018/01/1510116943014_APkNixj.mp4"}
+								width="100%"
+								height="100%"
+								src={this.state.video}
 
 							>
 							</video>
+							<Icon className="thirdIcon" 
+							onClick={this.play.bind(this)}
+							style={{ fontSize: 30, width: "30px", height: "30px", display: "none", position: "absolute", top: "0", bottom: "0", left: "0", right: "0", margin: "auto" }} name="play-circle"></Icon>
 						</Row>
 
 					</Col>
 
 
 
-				</Row>
+				</Row> */}
 
+				<div className="container">
+					<div className="box" style={{width:"60%",height:"300px",float:"left"}}>
+						<video
+							// onClick={this.play.bind(this)}
+							className="firstVideo"
+							preload="auto"
+							width="60%"
+							height="300px"
+							src={this.state.video}
+						>
+						</video>
+						<Icon className="firstIcon"
+							onClick={this.play.bind(this)}
+							style={{ fontSize: 60, width: "60px", height: "60px", display: "none"  }} name="play-circle"></Icon>
+					</div>
+					<div  className="box" style={{width:"40%",height:"150px", float:"right"}} >
+						<video
+							// onClick={this.play.bind(this)}
+							className="firstVideo"
+							preload="auto"
+							width="40%"
+							height="150px"
+							src={this.state.video}
+						>
+						</video>
+						<Icon className="firstIcon"
+							onClick={this.play.bind(this)}
+							style={{ fontSize: 30, width: "30px", height: "30px" }} name="play-circle"></Icon>
+					</div>
+					<div  className="box" style={{width:"40%",height:"150px",float:"right"}}>
+						<video
+							// onClick={this.play.bind(this)}
+							className="firstVideo"
+							preload="auto"
+							width="40%"
+							height="150px"
+							src={this.state.video}
+						>
+						</video>
+						<Icon className="firstIcon"
+							onClick={this.play.bind(this)}
+							style={{ fontSize: 30, width: "30px", height: "30px" }} name="play-circle"></Icon>
+					</div>
+				</div>
 
+				<Modal
+					visible={this.state.visible}
+					footer={null}
+					width="70%"
+					maskClosable={false}
+					onOk={this.modalClick.bind(this)}
+					onCancel={this.modalClick.bind(this)}
+				>
+					<video
+						controls
+						preload="auto"
+						width="100%"
+						height="500px"
+						src={this.state.video}
+					>
+					</video>
+				</Modal>
 			</Blade>
 		);
 	}
