f26388c47902d0bbac94bc201eca287620709ac0
寇晓阳 <654113183@qq.com>
2018-01-28 14:00:32 +0800

    【综合展示】更改路由配置

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,5 +1,34 @@
+// import {injectReducer} from '../store';
+// import React, {Component} from 'react';
+
+// export default class Dashboard extends Component {
+
+// 	async componentDidMount() {
+// 		const {default: reducer} = await import('./store');
+// 		const Containers = await import('./containers');
+// 		injectReducer('dashboard', reducer);
+// 		this.setState({
+// 			...Containers
+// 		});
+// 	}
+
+// 	render() {
+// 		const {Map = null} = this.state || {};
+// 		return (
+// 			<div style={{position: 'relative', width: '100%', height: 'calc( 100% - 80px )'}}>
+// 				{Map && <Map {...this.props}/>}
+// 			</div>)
+// 	}
+// };
+
+
+
 import {injectReducer} from '../store';
 import React, {Component} from 'react';
+import {Route,Switch} from 'react-router-dom';
+import {Aside, Body, Main, Layout} from '_platform/components/layout';
+import Submenu from '_platform/components/panels/Submenu';
+import {Icon} from 'react-fa';
 
 export default class Dashboard extends Component {
 
@@ -9,14 +38,47 @@ export default class Dashboard extends Component {
 		injectReducer('dashboard', reducer);
 		this.setState({
 			...Containers
-		});
+		})
+		debugger;
 	}
 
 	render() {
-		const {Map = null} = this.state || {};
+        const {OnSite, Project, Plan} = this.state || {};
 		return (
-			<div style={{position: 'relative', width: '100%', height: 'calc( 100% - 80px )'}}>
-				{Map && <Map {...this.props}/>}
-			</div>)
+			<div style={{display:"flex"}}>
+				<Aside style={{overflow:"hidden"}}>
+					<Submenu {...this.props} menus={Dashboard.menus} defaultOpenKeys={Dashboard.defaultOpenKeys}/>
+				</Aside>
+				<Main>
+					<Switch>
+						{OnSite && <Route exact path="/dashboard" component={OnSite}/>}
+						{OnSite && <Route path="/dashboard/onsite" component={OnSite}/>}
+						{Project && <Route path="/dashboard/project" component={Project}/>}
+						{Plan && <Route path="/dashboard/plan" component={Plan}/>}
+					</Switch>
+				</Main>
+			</div>);
 	}
-};
+
+	static menus = [{
+		key: 'ONSITE',
+		id: 'DASHBOARD.DASHBOARD',
+		path: '/dashboard/onsite',
+		name: '二维展示',
+        icon: <Icon name="map-o"/>,
+	}, {
+		key: 'PLAN',
+		id: 'DASHBOARD.PLAN',
+		path: '/dashboard/plan',
+		name: '规划信息',
+        icon: <Icon name="university"/>,
+	}, {
+		key: 'PROJECT',
+		id: 'DASHBOARD.PROJECT',
+		path: '/dashboard/project',
+		name: '项目信息',
+        icon: <Icon name="caret-square-o-up"/>,
+	}];
+	static defaultOpenKeys = ['ONSITE']
+}
+
