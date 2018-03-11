03938e4a9ec614b88a6ae4e6b14e258138fed748
xuxu <935141559@qq.com>
2018-03-11 16:52:22 +0800

    修改项目管理下的模块，完成树种管理模块的数据导入以及部分功能

--- src/Project/containers/TreeManage.js
+++ src/Project/containers/TreeManage.js
@@ -1,46 +1,36 @@
 import React, {Component} from 'react';
-import {Main, Aside, Body, Sidebar, Content, DynamicTitle} from '_platform/components/layout';
-import {actions as platformActions} from '_platform/store/global';
-import {actions} from '../store/treeManage';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import {SubTree,Info,ToggleModal} from '../components/treeManage';
-
+import {Main, Aside, Body, Sidebar, Content, DynamicTitle} from '_platform/components/layout';
+import {actions as platformActions} from '_platform/store/global';
+import reducer, {actions} from '../store/treeManage';
+import {Table} from '../components/TreeManage';
 @connect(
 	state => {
-		const {platform, project: {treeManage = {}} = {}} = state;
-		return {platform, ...treeManage};
+		const {project:{treeManage = {}}, platform} = state || {};
+		return {...treeManage, platform};
 	},
 	dispatch => ({
 		actions: bindActionCreators({...actions, ...platformActions}, dispatch),
 	}),
 )
-export default class treeManage extends Component {
 
+export default class TreeManage extends Component {
 	static propTypes = {};
-	componentDidMount() {
-		const {actions: {getFieldAc}} = this.props;
-		getFieldAc()
-	}
-	render() {
-		const {
-			toggleData: toggleData = {
-				type: null,
-			},
 
-		} = this.props;
+	render() {
 		return (
-			<div>
+			<div style={{
+				padding: 20, height: 'calc(100% - 37px)',
+				minHeight: '505px', 'overflowY': 'auto'
+			}}>
 				<DynamicTitle title="树种管理" {...this.props}/>
-				<Sidebar>
-					<SubTree {...this.props}/>
-				</Sidebar>
-				<Content>
-					<Info {...this.props}/>
-				</Content>
-				{
-					toggleData.type === null ? null : <ToggleModal {...this.props}/>
-				}
-			</div>);
+				<Table {...this.props}/>
+				{/* <Addition {...this.props}/> */}
+				{/* <Edite {...this.props}/> */}
+			</div>
+		);
 	}
+
+	
 }
