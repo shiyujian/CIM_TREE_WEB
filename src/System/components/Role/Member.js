c08955d2bc834253b65f69f95391580eb3d86dbd
lose <193382082@qq.com>
2018-03-12 16:52:07 +0800

    修改角色权限添加权限功能 注释首页质量信息统计列表

--- src/System/components/Role/Member.js
+++ src/System/components/Role/Member.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Modal, Table, Checkbox, Spin, Input,Row,Col } from 'antd';
+import { Modal, Table, Checkbox, Spin, Input, Row, Col } from 'antd';
+import { Button } from 'antd/lib/radio';
 
 export default class Member extends Component {
 	constructor(props) {
@@ -11,7 +12,8 @@ export default class Member extends Component {
 			search: false,
 			searchUser: [],
 			objIndex: {},
-			personIndex: {}
+			personIndex: {},
+			checkeds: ''
 		}
 	}
 	search(value) {
@@ -70,56 +72,56 @@ export default class Member extends Component {
 			}
 		});
 		return (
-			<Modal title={title} style={{top:0}} visible={visible} width="90%"
+			<Modal title={title} style={{ top: 0 }} visible={visible} width="90%"
 
 				onOk={this.ok.bind(this)} onCancel={this.cancel.bind(this)}>
 				<Spin spinning={this.props.getUserLoadings}>
 					{/* <div style={{ overflow: "scroll", height: "600px" }}> */}
-						<Row style={{marginBottom:'10px'}}>
-							<Col span={6}>
-								<h2 style={{ marginLeft: "10px"}}>已经关联的用户</h2>
-							</Col>
-							<Col span={6}>
-								<Search
-									placeholder="请输入搜索的名称"
-									style={{
-										width: "200px",
-										margin: "0 0 0 5px"
-									}}
-									onSearch={value => {
-										this.search.bind(this, value)()
-									}
-									}
-								/>
-							</Col>
-						</Row>
-						<Table bordered rowKey="id" style={{position:'relative'}} size='small' columns={this.columns1} dataSource={dataSource}
-						/>
+					<Row style={{ marginBottom: '10px' }}>
+						<Col span={6}>
+							<h2 style={{ marginLeft: "10px" }}>已经关联的用户</h2>
+						</Col>
+						<Col span={6}>
+							<Search
+								placeholder="请输入搜索的名称"
+								style={{
+									width: "200px",
+									margin: "0 0 0 5px"
+								}}
+								onSearch={value => {
+									this.search.bind(this, value)()
+								}
+								}
+							/>
+						</Col>
+					</Row>
+					<Table bordered rowKey="id" size='small' columns={this.columns1} dataSource={dataSource}
+					/>
 
-						<Row style={{position:'absolute',top:'400px',width:'100%'}}>
-							<Col span={6}>
-								<h2 style={{marginLeft: "10px"}}>所有用户</h2>
-							</Col>
-							<Col span={6} >
-								<Search
-									placeholder="请输入搜索的用户名"
-									style={{
-										width: "200px",
-										margin: "0 0 0 5px"
-									}}
-									onSearch={value => {
-										this.searchByName.bind(this, value)()
-									}
-									}
-								/>
-							</Col>
-						</Row>
-						<Table bordered rowKey="id" size='small' columns={this.columns} dataSource={this.state.searchUser ? (this.state.searchUser.length > 0 ? this.state.searchUser : users2) : users2}
-							onChange={this.changePage.bind(this)}
-							pagination={
-								this.state.searchUser ? (this.state.searchUser.length > 0 ? { current: 1, total: 1 } : this.props.getUserFristPages) : this.props.getUserFristPages
-							}
-						/>
+					<Row style={{ marginBottom: '10px' }}>
+						<Col span={6}>
+							<h2 style={{ marginLeft: "10px" }}>所有用户</h2>
+						</Col>
+						<Col span={6} >
+							<Search
+								placeholder="请输入搜索的用户名"
+								style={{
+									width: "200px",
+									margin: "0 0 0 5px"
+								}}
+								onSearch={value => {
+									this.searchByName.bind(this, value)()
+								}
+								}
+							/>
+						</Col>
+					</Row>
+					<Table bordered rowKey="id" size='small' columns={this.columns} dataSource={this.state.searchUser ? (this.state.searchUser.length > 0 ? this.state.searchUser : users2) : users2}
+						onChange={this.changePage.bind(this)}
+						pagination={
+							this.state.searchUser ? (this.state.searchUser.length > 0 ? { current: 1, total: 1 } : this.props.getUserFristPages) : this.props.getUserFristPages
+						}
+					/>
 					{/* </div> */}
 				</Spin>
 			</Modal>
@@ -149,16 +151,20 @@ export default class Member extends Component {
 			member: { members = [], role = {} } = {}
 		} = this.props;
 
-		// changeMemberField('visible', false);
-		if (this.props.getUserFristDatas) {
-			// changeMemberField('members', undefined);
-			console.log("11111111")
-			putMembers({ id: role.id }, { members }).then(rsts => {
-				getUserOK(rsts.members)
-				changeMemberField('members', rsts.members.map(member => member.id))
-			})
-		}
+		changeMemberField('visible', false);
+		// if (this.props.getUserFristDatas) {
+		// 	// changeMemberField('members', undefined);
+		// 	console.log("11111111")
+		// 	putMembers({ id: role.id }, { members }).then(rsts => {
+		// 		getUserOK(rsts.members)
+		// 		changeMemberField('members', rsts.members.map(member => member.id))
+		// 	})
+		// }
 	}
+	// cancelRelation() {
+
+
+	// }
 	cancel() {
 		const {
 			actions: { changeMemberField, getUserOK, getUsersPage, getUserFristPage, getUserFristData }
@@ -294,18 +300,114 @@ export default class Member extends Component {
 	}, {
 		title: '手机号码',
 		dataIndex: 'account.person_telephone',
+	}, {
+		title: '取消关联',
+		render: (user) => {
+			const { member = {} } = this.props;
+			const members = member.members || [];
+			const checked = members.some(member => member === user.id);
+			return <Checkbox checked={checked} onChange={this.cancelRelation.bind(this, user)} />
+
+		},
 	}];
 	// user是关联用户里面的其中一行记录
 
 	check(user) {
-		const { actions: { changeMemberField }, member: { members = [] } } = this.props;
+		console.log("22222222",user)
+		const { actions: { changeMemberField }, member: {role = [], members = [] } } = this.props;
 		const has = members.some(member => member === user.id);
 		let rst = [];
-		if (has) {
-			rst = members.filter(member => member !== user.id)
-		} else {
-			rst = [...members, user.id]
+		let groupsa = []
+		for (let i = 0; i < user.groups.length; i++) {
+			const element = user.groups[i];
+			if (has) {
+					console.log("1111111111")
+				rst = members.filter(member => member !== user.id)
+				if (element.id == role.id) {
+					console.log("222222")
+					
+				} else {
+					console.log("333333")
+					
+					groupsa.push(element.id)
+				}
+			} else {
+				console.log("4444444444")
+				
+				rst = [...members, user.id]
+				groupsa.push(role.id)
+				groupsa.push(element.id)
+			}
+			changeMemberField('members', rst);
 		}
-		changeMemberField('members', rst);
+		console.log("groupsa", groupsa)
+
+	}
+	cancelRelation(user) {
+		const { actions: { changeMemberField }, member: { role = [], members = [] } } = this.props;
+		const has = members.some(member => member === user.id);
+		let rst = [];
+		let groupsa = []
+		for (let i = 0; i < user.groups.length; i++) {
+			const element = user.groups[i];
+			if (has) {
+				rst = members.filter(member => member !== user.id)
+				if (element.id == role.id) {
+
+				} else {
+					groupsa.push(element.id)
+				}
+			} else {
+				rst = [...members, user.id]
+				groupsa.push(element.id)
+			}
+			changeMemberField('members', rst);
+
+		}
+		console.log("groupsa", groupsa)
+		const {
+			actions: { putUser, getOrgName }
+		} = this.props;
+		// return
+		getOrgName({ code: user.account.org_code }).then(items => {
+			putUser({}, {
+				id: user.id,
+				username: user.username,
+				email: user.email,
+				// password: addition.password, // 密码不能变？信息中没有密码
+				account: {
+					person_name: user.person_name,
+					person_type: "C_PER",
+					person_avatar_url: "",
+					organization: {
+						pk: items.pk,
+						code: user.account.org_code,
+						obj_type: "C_ORG",
+						rel_type: "member",
+						name: user.account.organization
+					},
+				},
+				tags: user.account.tags,
+				sections: user.account.sections,
+				//groups: [7],
+				groups: groupsa,
+				is_active: true,
+				basic_params: {
+					info: {
+						'电话': user.account.person_telephone || '',
+						'性别': user.account.gender || '',
+						'技术职称': user.account.title || '',
+						'phone': user.account.person_telephone || '',
+						'sex': user.account.gender || '',
+						'duty': ''
+					}
+				},
+				extra_params: {},
+				title: user.account.title || ''
+
+			}).then(rst => {
+
+			})
+		})
 	}
 }
