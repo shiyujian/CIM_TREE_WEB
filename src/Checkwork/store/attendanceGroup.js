45e27031fe5dd422dd5c63ea6e4efdffd959c35b
mingey <462638868@qq.com>
2018-09-18 17:32:37 +0800

    考勤管理redux设置

--- src/Checkwork/store/attendanceGroup.js
+++ src/Checkwork/store/attendanceGroup.js
@@ -4,6 +4,19 @@ import createFetchAction from 'fetch-action';
 import {
     FOREST_API
 } from '_platform/api';
+export const ID = 'Checkwork_attendancegroup';
+// 示例action
+// 苗木养护查询
+export const getCuring = createFetchAction(`${FOREST_API}/curing/curings`, [], 'GET');
+// 修改选择地图的方式
+export const changeSelectMap = createAction(`${ID}_changeSelectMap`);
 
+export const actions = {
+    getCuring
+};
 export default handleActions({
+    [changeSelectMap]: (state, {payload}) => ({
+        ...state,
+        selectMap: payload
+    })
 }, {});
