a95120275fb5ecb41536122fb844e5b998b3275c
syl <zyl793988208@qq.com>
2018-09-25 18:17:21 +0800

    考勤管理统计分析列表数据获取以及查询功能

--- src/Checkwork/store/attendanceCount.js
+++ src/Checkwork/store/attendanceCount.js
@@ -2,17 +2,20 @@ import { createAction, handleActions } from 'redux-actions';
 import createFetchAction from 'fetch-action';
 
 import {
-    FOREST_API
+    FOREST_API,
+    base
 } from '_platform/api';
 export const ID = 'Checkwork_attendancecount';
 // 示例action
-// 苗木养护查询
-export const getCuring = createFetchAction(`${FOREST_API}/curing/curings`, [], 'GET');
+
 // 修改选择地图的方式
 export const changeSelectMap = createAction(`${ID}_changeSelectMap`);
 
+// 考勤列表
+export const getCheckRecord = createFetchAction(`${base}/main/api/check-record/list/?{{params}}`, [], 'GET')
+
 export const actions = {
-    getCuring
+    getCheckRecord
 };
 export default handleActions({
     [changeSelectMap]: (state, {payload}) => ({
