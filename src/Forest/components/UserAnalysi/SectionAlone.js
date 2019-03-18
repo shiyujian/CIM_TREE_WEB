26d0b67a2b3721fc9888907ef90062366655d1f2
mingey <462638868@qq.com>
2019-03-18 21:46:55 +0800

    1.完成苗木来源地分析页面全国，河北省，保定市三个图表的搜索展示以及数据导出功能 2.完成用户分析模块账号总览，新增账号，活跃账号等图表的统计展示 3.完成用户分析模块标段分览，施工监理各自的账号注册总数和日活跃账号数

--- src/Forest/components/UserAnalysi/SectionAlone.js
+++ src/Forest/components/UserAnalysi/SectionAlone.js
@@ -11,65 +11,42 @@ export default class SectionAlone extends Component {
     constructor (props) {
         super(props);
         this.state = {
-            treeList: [], // 树列表数据
             sectionObjList: [], // 标段对象列表
-            jobSumDate: '', // 施工账号注册总数日期
-            jobNumDate: '', // 施工日活跃账号数日期
-            superviseSumDate: '', // 监理账号注册总数日期
-            superviseNumDate: '', // 监理日活跃账号数日期
+            jobSumDate: moment().format('YYYY-MM-DD'), // 施工账号注册总数日期
+            jobNumDate: moment().format('YYYY-MM-DD'), // 施工日活跃账号数日期
+            superviseSumDate: moment().format('YYYY-MM-DD'), // 监理账号注册总数日期
+            superviseNumDate: moment().format('YYYY-MM-DD'), // 监理日活跃账号数日期
             leftkeycode: '' // 选择项目code
         };
-        this.handleDate = this.handleDate.bind(this); // 切换截至日期1
-        this.handleDateTwo = this.handleDateTwo.bind(this); // 切换截至日期2
-        this.handleDateThree = this.handleDateThree.bind(this); // 切换截至日期3
-        this.handleDateFour = this.handleDateFour.bind(this); // 切换截至日期4
-        this.renderJobRegisterSum = this.renderJobRegisterSum.bind(this); // 渲染施工账号注册总数图表
-        this.renderJobDynamicNum = this.renderJobDynamicNum.bind(this); // 渲染日活跃账号数图表
-        this.renderSuperviseRegisterSum = this.renderSuperviseRegisterSum.bind(this); // 渲染监理账号注册总数图表
-        this.renderSuperviseDynamicNum = this.renderSuperviseDynamicNum.bind(this); // 渲染监理日活跃账号数图表
     }
 
     componentDidMount = async () => {
         const {
             actions: {
-                getTreeNodeList,
-                getThinClassList,
-                getThinClassTree
+                getTreeNodeList
             },
             platform: { tree = {} }
         } = this.props;
         try {
-            if (!(tree && tree.thinClassTree && tree.thinClassTree instanceof Array && tree.thinClassTree.length > 0)) {
-                let data = await getAreaTreeData(getTreeNodeList, getThinClassList);
-                let projectList = data.projectList || [];
-                // 区域地块树
-                await getThinClassTree(projectList);
+            if (!(tree && tree.bigTreeList && tree.bigTreeList instanceof Array && tree.bigTreeList.length > 0)) {
+                await getTreeNodeList();
             }
             let defaultProject = await getDefaultProject();
             console.log('defaultProject', defaultProject);
             if (defaultProject) {
-                this.onSelect([defaultProject]);
+                await this.onSelect([defaultProject]);
             }
-            this.renderJobRegisterSum();
-            this.renderJobDynamicNum();
-            this.renderSuperviseRegisterSum();
-            this.renderSuperviseDynamicNum();
         } catch (e) {
             console.log('e', e);
         }
     }
-    componentWillReceiveProps (nextProps) {
-        const { tree } = nextProps.platform;
-        if (tree) {
-            console.log('componentWillReceiveProps', nextProps.platform.tree);
-            this.setState({
-                treeList: tree.bigTreeList
-            });
-        }
-    }
 
     render () {
-        const { leftkeycode, treeList } = this.state;
+        const {
+            platform: { tree = {} }
+        } = this.props;
+        const { leftkeycode } = this.state;
+        let treeList = (tree && tree.bigTreeList) || [];
         return (
             <div>
                 <Sidebar width={190}>
@@ -87,7 +64,16 @@ export default class SectionAlone extends Component {
                         <div style={{ background: '#ECECEC', padding: '30px', height: 550 }}>
                             <Row gutter={16}>
                                 <Col span={12}>
-                                    <Card title='账号注册总数' bordered={false} extra={<span>截至日期：<DatePicker defaultValue={moment()} onChange={this.handleDate.bind(this)} /></span>}>
+                                    <Card title='账号注册总数'
+                                        bordered={false}
+                                        extra={
+                                            <span>
+                                            截至日期：
+                                                <DatePicker
+                                                    defaultValue={moment()}
+                                                    onChange={this.handleDate.bind(this)}
+                                                />
+                                            </span>}>
                                         <div
                                             id='jobRegisterSum'
                                             style={{ width: '100%', height: '350px' }}
@@ -95,7 +81,16 @@ export default class SectionAlone extends Component {
                                     </Card>
                                 </Col>
                                 <Col span={12}>
-                                    <Card title='日活跃账号数' bordered={false} extra={<span>截至日期：<DatePicker defaultValue={moment()} onChange={this.handleDateTwo.bind(this)} /></span>}>
+                                    <Card title='日活跃账号数'
+                                        bordered={false}
+                                        extra={
+                                            <span>
+                                            截至日期：
+                                                <DatePicker
+                                                    defaultValue={moment()}
+                                                    onChange={this.handleDateTwo.bind(this)}
+                                                />
+                                            </span>}>
                                         <div
                                             id='jobDynamicNum'
                                             style={{ width: '100%', height: '350px' }}
@@ -110,7 +105,16 @@ export default class SectionAlone extends Component {
                         <div style={{ background: '#ECECEC', padding: '30px', height: 550 }}>
                             <Row gutter={16}>
                                 <Col span={12}>
-                                    <Card title='账号注册总数' bordered={false} extra={<span>截至日期：<DatePicker defaultValue={moment()} onChange={this.handleDateThree.bind(this)} /></span>}>
+                                    <Card title='账号注册总数'
+                                        bordered={false}
+                                        extra={
+                                            <span>
+                                        截至日期：
+                                                <DatePicker
+                                                    defaultValue={moment()}
+                                                    onChange={this.handleDateThree.bind(this)}
+                                                />
+                                            </span>}>
                                         <div
                                             id='superviseRegisterSum'
                                             style={{ width: '100%', height: '350px' }}
@@ -118,7 +122,16 @@ export default class SectionAlone extends Component {
                                     </Card>
                                 </Col>
                                 <Col span={12}>
-                                    <Card title='日活跃账号数' bordered={false} extra={<span>截至日期：<DatePicker defaultValue={moment()} onChange={this.handleDateFour.bind(this)} /></span>}>
+                                    <Card title='日活跃账号数'
+                                        bordered={false}
+                                        extra={
+                                            <span>
+                                         截至日期：
+                                                <DatePicker
+                                                    defaultValue={moment()}
+                                                    onChange={this.handleDateFour.bind(this)}
+                                                />
+                                            </span>}>
                                         <div
                                             id='superviseDynamicNum'
                                             style={{ width: '100%', height: '350px' }}
@@ -132,9 +145,12 @@ export default class SectionAlone extends Component {
             </div>
         );
     }
-    onSelect (keys) {
-        const { treeList } = this.state;
-        console.log('selct', treeList);
+    onSelect = async (keys) => {
+        const {
+            platform: { tree = {} }
+        } = this.props;
+        let treeList = (tree && tree.bigTreeList) || [];
+        console.log('treeList', treeList);
         let leftkeycode = keys[0] || '';
         let sectionObjList = [];
         treeList.map(item => {
@@ -143,9 +159,14 @@ export default class SectionAlone extends Component {
             }
         });
         console.log(leftkeycode, '123');
-        this.setState({
+        await this.setState({
             leftkeycode,
             sectionObjList
+        }, async () => {
+            await this.renderJobRegisterSum();
+            await this.renderJobDynamicNum();
+            await this.renderSuperviseRegisterSum();
+            await this.renderSuperviseDynamicNum();
         });
     }
     // 切换标签页
@@ -161,15 +182,40 @@ export default class SectionAlone extends Component {
             this.renderJobRegisterSum();
         });
     }
-    renderJobRegisterSum () {
-        const { sectionObjList, leftkeycode } = this.state;
-        console.log('treeList', sectionObjList, leftkeycode);
+    renderJobRegisterSum = async () => {
+        const { sectionObjList, jobSumDate } = this.state;
+        const {
+            actions: {
+                getSectionUserStat
+            }
+        } = this.props;
+        let postData = {
+            unittype: 'construction',
+            stime: '',
+            etime: moment(jobSumDate).format('YYYY-MM-DD')
+        };
+        let data = await getSectionUserStat({}, postData);
+        console.log('data', data);
         let xAxisArr = [];
+        let yGrandData = [];
         sectionObjList.map(item => {
-            xAxisArr.push(item.Name);
+            data.map((sectionData) => {
+                if (sectionData && sectionData.Section) {
+                    if (sectionData.Section === item.No) {
+                        xAxisArr.push(item.Name);
+                        yGrandData.push(sectionData.Num);
+                    }
+                }
+            });
         });
         let myChart = echarts.init(document.getElementById('jobRegisterSum'));
         let option = {
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: { // 坐标轴指示器，坐标轴触发有效
+                    type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+                }
+            },
             xAxis: {
                 type: 'category',
                 data: xAxisArr
@@ -178,8 +224,9 @@ export default class SectionAlone extends Component {
                 type: 'value'
             },
             series: [{
-                data: [120, 200, 150, 80, 70, 110, 130],
-                type: 'bar'
+                data: yGrandData,
+                type: 'bar',
+                name: '账号注册总数'
             }]
         };
         myChart.setOption(option);
@@ -191,19 +238,54 @@ export default class SectionAlone extends Component {
             this.renderJobDynamicNum();
         });
     }
-    renderJobDynamicNum () {
+    renderJobDynamicNum = async () => {
+        const {
+            actions: {
+                getSectionUserStat
+            }
+        } = this.props;
+        const {
+            jobNumDate,
+            sectionObjList
+        } = this.state;
+        let postData = {
+            unittype: 'construction',
+            stime: '',
+            etime: moment(jobNumDate).format('YYYY-MM-DD')
+        };
+        let data = await getSectionUserStat({}, postData);
+        console.log('data', data);
+        let xAxisArr = [];
+        let yGrandData = [];
+        sectionObjList.map(item => {
+            data.map((sectionData) => {
+                if (sectionData && sectionData.Section) {
+                    if (sectionData.Section === item.No) {
+                        xAxisArr.push(item.Name);
+                        yGrandData.push(sectionData.Num);
+                    }
+                }
+            });
+        });
         let myChart = echarts.init(document.getElementById('jobDynamicNum'));
         let option = {
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: { // 坐标轴指示器，坐标轴触发有效
+                    type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+                }
+            },
             xAxis: {
                 type: 'category',
-                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                data: xAxisArr
             },
             yAxis: {
                 type: 'value'
             },
             series: [{
-                data: [120, 200, 150, 80, 70, 110, 130],
-                type: 'bar'
+                data: yGrandData,
+                type: 'bar',
+                name: '日活跃账户数'
             }]
         };
         myChart.setOption(option);
@@ -215,19 +297,54 @@ export default class SectionAlone extends Component {
             this.renderSuperviseRegisterSum();
         });
     }
-    renderSuperviseRegisterSum () {
+    renderSuperviseRegisterSum = async () => {
+        const {
+            actions: {
+                getSectionUserStat
+            }
+        } = this.props;
+        const {
+            superviseSumDate,
+            sectionObjList
+        } = this.state;
+        let postData = {
+            unittype: 'supervisor',
+            stime: '',
+            etime: moment(superviseSumDate).format('YYYY-MM-DD')
+        };
+        let data = await getSectionUserStat({}, postData);
+        console.log('data', data);
+        let xAxisArr = [];
+        let yGrandData = [];
+        sectionObjList.map(item => {
+            data.map((sectionData) => {
+                if (sectionData && sectionData.Section) {
+                    if (sectionData.Section === item.No) {
+                        xAxisArr.push(item.Name);
+                        yGrandData.push(sectionData.Num);
+                    }
+                }
+            });
+        });
         let myChart = echarts.init(document.getElementById('superviseRegisterSum'));
         let option = {
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: { // 坐标轴指示器，坐标轴触发有效
+                    type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+                }
+            },
             xAxis: {
                 type: 'category',
-                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                data: xAxisArr
             },
             yAxis: {
                 type: 'value'
             },
             series: [{
-                data: [120, 200, 150, 80, 70, 110, 130],
-                type: 'bar'
+                data: yGrandData,
+                type: 'bar',
+                name: '账号注册总数'
             }]
         };
         myChart.setOption(option);
@@ -239,19 +356,54 @@ export default class SectionAlone extends Component {
             this.renderSuperviseDynamicNum();
         });
     }
-    renderSuperviseDynamicNum () {
+    renderSuperviseDynamicNum = async () => {
+        const {
+            actions: {
+                getSectionUserStat
+            }
+        } = this.props;
+        const {
+            superviseNumDate,
+            sectionObjList
+        } = this.state;
+        let postData = {
+            unittype: 'supervisor',
+            stime: '',
+            etime: moment(superviseNumDate).format('YYYY-MM-DD')
+        };
+        let data = await getSectionUserStat({}, postData);
+        console.log('data', data);
+        let xAxisArr = [];
+        let yGrandData = [];
+        sectionObjList.map(item => {
+            data.map((sectionData) => {
+                if (sectionData && sectionData.Section) {
+                    if (sectionData.Section === item.No) {
+                        xAxisArr.push(item.Name);
+                        yGrandData.push(sectionData.Num);
+                    }
+                }
+            });
+        });
         let myChart = echarts.init(document.getElementById('superviseDynamicNum'));
         let option = {
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: { // 坐标轴指示器，坐标轴触发有效
+                    type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+                }
+            },
             xAxis: {
                 type: 'category',
-                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                data: xAxisArr
             },
             yAxis: {
                 type: 'value'
             },
             series: [{
-                data: [120, 200, 150, 80, 70, 110, 130],
-                type: 'bar'
+                data: yGrandData,
+                type: 'bar',
+                name: '日活跃账户数'
             }]
         };
         myChart.setOption(option);
