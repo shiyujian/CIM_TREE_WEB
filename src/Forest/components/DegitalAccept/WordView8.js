32be6067a55e15a76df7359217cee44762ab5a98
mingey <462638868@qq.com>
2019-12-14 11:03:19 +0800

    1. 修改数字化验收表单中问题上报数据内容

--- src/Forest/components/DegitalAccept/WordView8.js
+++ src/Forest/components/DegitalAccept/WordView8.js
@@ -11,8 +11,7 @@ export default class WordView1 extends Component {
             leader: '',
             unitName: '',
             detail: '',
-            unQualifiedList: [],
-            eventStat: ''
+            unQualifiedList: []
         };
     }
 
@@ -22,35 +21,12 @@ export default class WordView1 extends Component {
         } = this.props;
         if (itemDetailList.length > 0) {
             let detail = itemDetailList[0];
-            await this.getEventStat(detail);
             await this.getUnqualifiedList(detail);
             await this.getUnitMessage(detail);
             this.setState({
                 detail
             });
         }
-    }
-    getEventStat = async (detail) => {
-        const {
-            actions: {
-                getAcceptAnceEventStat
-            }
-        } = this.props;
-        this.setState({
-            loading: true
-        });
-        let postdata = {
-            section: detail.Section,
-            thinclass: detail.ThinClass,
-            // treetype: detail.TreeType,
-            eventtype: 0,
-            problemtype: '土堰直径不满足要求,土堰深度不满足要求,首次浇水不及时、未浇透,浇水后未封穴或封穴不密实'
-        };
-        let result = await getAcceptAnceEventStat({}, postdata);
-        this.setState({
-            loading: false,
-            eventStat: (result) || ''
-        });
     }
     getUnqualifiedList = async (detail) => {
         const {
@@ -137,9 +113,6 @@ export default class WordView1 extends Component {
         });
     }
     handleDetailData = (detail) => {
-        const {
-            eventStat
-        } = this.state;
         let handleDetail = {};
         handleDetail.unit = (detail && detail.AcceptanceObj && detail.AcceptanceObj.Land) || '';
         handleDetail.jianli = (detail && detail.AcceptanceObj && detail.AcceptanceObj.SupervisorObj && detail.AcceptanceObj.SupervisorObj.Full_Name) || '';
@@ -153,8 +126,8 @@ export default class WordView1 extends Component {
         handleDetail.actualNum = (detail && detail.ActualNum) || 0;
         handleDetail.loftingNum = (detail && detail.LoftingNum) || 0;
         handleDetail.score = (detail && detail.Score && (detail.Score).toFixed(2)) || 0;
-        handleDetail.checkNum = (eventStat && eventStat.SurpervisorNum) || 0;
-        handleDetail.failedNum = (eventStat && eventStat.NoQualifiedNum) || 0;
+        handleDetail.checkNum = (detail && detail.CheckNum) || 0;
+        handleDetail.failedNum = (detail && detail.FailedNum) || 0;
         handleDetail.treetypename = (detail && detail.TreeTypeObj && detail.TreeTypeObj.TreeTypeName) || '';
 
         let hgl = handleDetail.checkNum - handleDetail.failedNum; // 合格量
