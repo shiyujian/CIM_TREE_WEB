b8fdad478c74e5b10724759c194935858ae3cf8e
mingey <462638868@qq.com>
2020-07-24 11:18:00 +0800

    修改组织机构编辑功能 雄县7标开放权限

--- src/Forest/components/DegitalAccept/DegitalAcceptTable.js
+++ src/Forest/components/DegitalAccept/DegitalAcceptTable.js
@@ -166,7 +166,6 @@ export default class DegitalAcceptTable extends Component {
             {
                 title: '操作',
                 render: (text, record) => {
-                    console.log('状态', record);
                     const {
                         userOperatePermission
                     } = this.props;
@@ -180,7 +179,6 @@ export default class DegitalAcceptTable extends Component {
                     if ((duty && duty === '施工整改人') || user.username === 'admin') {
                         permission = true;
                     }
-                    console.log('filterList', filterList);
                     if (record.CheckType && record.CheckType === 10) {
                         // 验收类型：造林面积
                         let status = true;
@@ -467,7 +465,7 @@ export default class DegitalAcceptTable extends Component {
                                         <a onClick={this.againCheck.bind(this, 'permission')}>重新验收</a>
                                     </div>);
                                 }
-                            } else{
+                            } else {
                                 if (permission) {
                                     if (record.CanReAccpetance === 1) {
                                         return (<div >
